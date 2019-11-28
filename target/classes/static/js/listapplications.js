b885344fc7a7fb7907ae4b899970f0ef00883551
Konstantinos Vaismenos <it21606@hua.gr>
2019-11-28 23:59:24 +0200

    Fixed json return and added applications list

---
+++ target/classes/static/js/listapplications.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+    $('#example').DataTable( {
+        ajax: {
+            url: '/api/all',
+            type: 'GET',
+            dataType: 'json',
+            dataSrc: ''
+        },
+        columns: [
+            { data: 'user_id' },
+            { data: 'income' },
+            { data: 'bothParentsUnemployed' , "render": checkbox},
+            { data: 'hasSiblings' , "render": checkbox},
+            { data: 'hasSiblingsInOtherCities', "render": checkbox }
+
+        ]
+    } );
+
+     function checkbox ( data, type, full, meta ) {
+        var is_checked = data === true ? "checked" : "";
+        return '<input type="checkbox" class="checkbox" ' +
+            is_checked + ' />';
+    }
+});
