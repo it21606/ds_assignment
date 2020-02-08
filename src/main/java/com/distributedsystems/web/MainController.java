e79c5def4aec9c144fc2e8aa6718182d8c54eafd
manos <manos8991@gmail.com>
2020-02-08 14:43:08 +0200

    changePassword.html

--- src/main/java/com/distributedsystems/web/MainController.java
+++ src/main/java/com/distributedsystems/web/MainController.java
@@ -3,12 +3,15 @@ package com.distributedsystems.web;
 import com.distributedsystems.model.User;
 import com.distributedsystems.service.ApplicationParametersService;
 import com.distributedsystems.service.UserService;
+import com.distributedsystems.web.viewmodel.ChangePasswordViewModel;
 import com.distributedsystems.web.viewmodel.UserViewModel;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -56,4 +59,18 @@ public class MainController {
         return "profile";
     }
 
+
+    @GetMapping("/changePassword")
+    public String changePassword(Model model) {
+        ChangePasswordViewModel passwordVM = new ChangePasswordViewModel(null, null, null);
+        model.addAttribute("passwordVM", passwordVM);
+        return "changePassword";
+    }
+
+    @PostMapping("/changePassword")
+    public String changePassword(@ModelAttribute("passwordVM") ChangePasswordViewModel passwordVM) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "redirect:/changePassword?success";
+    }
 }
